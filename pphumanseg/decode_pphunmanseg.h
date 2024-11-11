448fa4b92649621ef05481685d33a3dd6d4a989f
tayct <tay.cu@gremsy.com>
2024-11-11 16:24:56 +0700

    change src

--- pphumanseg/decode_pphunmanseg.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include"../utils/utils.h"
-#include"../utils/common_include.h"
-
-namespace pphunmanseg
-{
-	void decodeDevice(int batchSize, float* src, int srcWidth, int srcHeight, float* dst, int dstWidth, int dstHeight);
-}
--- yolov4/decode_yolov4.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include"../utils/utils.h"
-#include"../utils/kernel_function.h"
-
-namespace yolov4
-{
-	void decodeDevice(utils::InitParameter param, float* src, int srcWidth, int srcHeight, int srcLength, float* dst, int dstWidth, int dstHeight);
-}
